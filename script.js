5e26a5fc98eb0231d55d565726cbe7b48b6d94b2
pedim08 <pedrophnobre2000@gmail.com>
2026-09-22 15:52:31 -0300

    Remove image URL from GPU data
    Removed image URL for GeForce RTX 3060.

--- script.js
+++ script.js
@@ -125,7 +125,6 @@ const products = [
         categoryName: "Placa de Vídeo",
         name: "GeForce RTX 3060 12GB GDDR6",
         price: 1999.90,
-        imagem:"https://images.unsplash.com/photo-1727895949000-da3c10a7d562?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         oldPrice: 2299.90,
         offer: true
     },
