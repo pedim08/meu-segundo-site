5334ef615cbf74091080adba7b9b6d1dbb547083
pedim08 <pedrophnobre2000@gmail.com>
2026-09-22 16:33:27 -0300

    Change image URL for GeForce RTX 3060
    Updated the image URL for the GeForce RTX 3060.

--- script.js
+++ script.js
@@ -120,14 +120,16 @@ const products = [
     ====================================================== */
 
     {
-        id: 11,
-        category: "gpu",
-        categoryName: "Placa de Vídeo",
-        name: "GeForce RTX 3060 12GB GDDR6",
-        image:"https://images.unsplash.com/photo-1727895949000-da3c10a7d562?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",    
-         price: 1999.90, 
-        oldPrice: 2299.90,
-        offer: true
+      {
+    id: 11,
+    category: "gpu",
+    categoryName: "Placa de Vídeo",
+    name: "GeForce RTX 3060 12GB GDDR6",
+    image: "https://images.unsplash.com/photo-1587202372775-e229f172b9d7?w=600",
+    price: 1999.90,
+    oldPrice: 2299.90,
+    offer: true
+  },
     },
 
     {
